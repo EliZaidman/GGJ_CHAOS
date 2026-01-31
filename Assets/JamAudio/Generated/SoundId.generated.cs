cf48a65fe1e30926f3f5a1a0799843a0aa5028ca
BenPanski <benpanski@gmail.com>
2026-01-31 13:41:18 +0200

    bens adss some sounds (money , victory)

--- Assets/JamAudio/Generated/SoundId.generated.cs
+++ Assets/JamAudio/Generated/SoundId.generated.cs
@@ -1,5 +1,5 @@
 ﻿// AUTO-GENERATED. Do not edit by hand.
-// Generated at: 2026-01-31 11:36:29
+// Generated at: 2026-01-31 13:27:51
 // Source of truth: SoundLibrary.cues (auto-filled from all SoundCues in project).
 
 public enum SoundId
@@ -13,11 +13,10 @@ public enum SoundId
     JumpAirWhoosh = 6,
     JumpCharacterWee = 7,
     MoneyGained = 8,
-    MoneyGained_2 = 9,
-    Lobby = 10,
-    music1 = 11,
-    RoundStart = 12,
-    UIClick = 13,
-    VictoryMexico = 14,
-    WallBreak = 15,
+    Lobby = 9,
+    music1 = 10,
+    RoundStart = 11,
+    UIClick = 12,
+    VictoryMexico = 13,
+    WallBreak = 14,
 }
